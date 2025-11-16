e21e4e039eeb99c303d99300567026bb7d6b8681
teuku munawar <munawartmk@gmail.com>
2025-11-16 12:20:53 +0700

    chore: standardize Kafka config in user-service
    - Switch producer prefix to spring.kafka.producer
    - Add KafkaConsumerProperties with full type safety
    - Refactor KafkaConfig to remove @Value and use config props
    - Add consumerConfigs() bean + configurable pollTimeout
    - Update YAML configs (base/dev/prod) to unified spring.kafka.*
    - Remove all orange.kafka.producer usages

---
+++ user-service/src/main/java/com/bni/orange/users/config/properties/KafkaConsumerProperties.java
@@ -0,0 +1,52 @@
+package com.bni.orange.users.config.properties;
+
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+import org.springframework.validation.annotation.Validated;
+
+@Validated
+@ConfigurationProperties(prefix = "spring.kafka.consumer")
+public record KafkaConsumerProperties(
+    @NotBlank
+    @DefaultValue("localhost:9092")
+    String bootstrapServers,
+
+    @NotBlank
+    @DefaultValue("user-service")
+    String groupId,
+
+    @NotBlank
+    @DefaultValue("org.apache.kafka.common.serialization.StringDeserializer")
+    String keyDeserializer,
+
+    @NotBlank
+    @DefaultValue("org.apache.kafka.common.serialization.ByteArrayDeserializer")
+    String valueDeserializer,
+
+    @NotBlank
+    @DefaultValue("earliest")
+    String autoOffsetReset,
+
+    @DefaultValue("false")
+    boolean enableAutoCommit,
+
+    @DefaultValue("100")
+    int maxPollRecords,
+
+    @DefaultValue("300000")
+    int maxPollIntervalMs,
+
+    @DefaultValue("30000")
+    int sessionTimeoutMs,
+
+    @DefaultValue("10000")
+    int heartbeatIntervalMs,
+
+    @DefaultValue("3")
+    int concurrency,
+
+    @DefaultValue("3000")
+    long pollTimeout
+) {
+}
